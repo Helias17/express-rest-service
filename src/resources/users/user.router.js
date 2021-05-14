f9d663750273766f5b4506775de4d5544b1dcb6a
Ilya Komar <helias@yandex.ru>
2021-05-15 00:17:56 +0300

    feat: add features getAllUsers, createUser, getUserById

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
-const User = require('./user.model');
 const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  res.json(users);
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.getUserById(req.params.id);
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).send('Not found');
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  const createdUser = await usersService.createUser(req.body);
+  res.status(201).json(createdUser);
 });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,23 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
 
-const getAll = () => usersRepo.getAll();
+const getAll = async () => usersRepo.users.map(User.toResponse);
 
-module.exports = { getAll };
+const createUser = async (name, login, password) => {
+  const newUser = new User(name, login, password);
+  usersRepo.users.push(newUser);
+  return User.toResponse(newUser);
+};
+
+const getUserById = async (id) => {
+  const foundUser = usersRepo.users.find( user => {
+    console.log('user.id', user.id);
+    console.log('id', id);
+    return user.id === id;
+  });
+  console.log(foundUser);
+  return foundUser ? User.toResponse(foundUser) : null;
+};
+
+
+module.exports = { getAll, createUser, getUserById };
