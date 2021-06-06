69bc209c44cecb852b304edd1e925347ac47d68d
Ilya Komar <helias@yandex.ru>
2021-06-06 23:01:37 +0300

    feat: add logging in central error handler

--- src/middleware/errorsMiddleware.ts
+++ src/middleware/errorsMiddleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction, } from 'express';
 import { handleError } from '../services/errors/handleError';
 import { ErrorHandler } from '../services/errors/ErrorHandler';
+import { logger } from '../services/logger';
 
 export const errorsMiddleware = (
   err: ErrorHandler,
@@ -8,4 +9,11 @@ export const errorsMiddleware = (
   res: Response,
   _next: NextFunction): void => {
   handleError(err, res);
+
+  const logErr = {
+    level: 'error',
+    ...err
+  }
+
+  logger.log(logErr);
 }
