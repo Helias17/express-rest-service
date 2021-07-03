f425497964cf2149ea24f9708e0307193447d887
Ilya Komar <helias@yandex.ru>
2021-07-03 18:53:06 +0300

    feat: add user admin after tables created

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 import { createConnection, QueryRunner } from 'typeorm';
+import UserEntity from './entity/User';
 import { PORT } from './common/config';
 import { app } from './app';
 import Migration from './db/migration/1624976310240-trello';
+const User = require('./resources/users/user.model');
+const bcrypt = require('bcrypt');
 
 const trelloMigration = new Migration();
 
@@ -10,10 +13,17 @@ createConnection().then(async (connection) => {
 
   const queryRunner: QueryRunner = connection.createQueryRunner();
   await queryRunner.connect();
-  const tables = await queryRunner.getTables(['Columns', 'Boards', 'Users', 'Tasks'])
+  const tables = await queryRunner.getTables(['Columns', 'Boards', 'Users', 'Tasks']);
 
   if (!tables.length) {
     await trelloMigration.up(queryRunner);
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash('admin', saltRounds);
+
+    const userRepo = connection.manager.getRepository(UserEntity);
+    const newUser = new User({ name: 'admin', login: 'admin', password: passwordHash });
+    await userRepo.save(newUser);
   }
 
   app.listen(PORT, () => {
