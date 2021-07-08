ab95be1f1a37a97fc0be12c1f7deca5a3ab61641
Ilya Komar <helias@yandex.ru>
2021-07-08 13:54:18 +0300

    feat: add swagger for nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import YAML from 'yamljs';
+import path from 'path';
 import { AppModule } from './app.module';
 import { PORT } from './common/config';
 import { createTables } from './utils/createTables';
@@ -6,6 +9,10 @@ import { createTables } from './utils/createTables';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await createTables();
+
+  const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   await app.listen(PORT!);
 }
 bootstrap();
